e5d0e5bcea3d7031cfbe2a42fec6b09de571b304
Julien Grossiord <jgrossiord@mediastay.com>
2013-05-30 19:03:53 +0200

    DB init

---
+++ script/db-init.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE IF NOT EXISTS silex;
+USE silex;
+DROP PROCEDURE IF EXISTS Init;
+
+DELIMITER //
+
+CREATE PROCEDURE Init()
+
+BEGIN
+    DECLARE USER_EXISTS, TABLE_POPULATED integer;
+
+    SET USER_EXISTS = (SELECT 1 FROM mysql.user WHERE User = 'silex' AND Host = '%');
+    SET TABLE_POPULATED = (SELECT count(*) FROM silex.users);
+
+    IF USER_EXISTS IS NULL THEN 
+        CREATE USER 'silex'@'%' IDENTIFIED BY 'silex';
+        GRANT SELECT,INSERT,UPDATE,DELETE ON silex.* TO 'silex'@'%';
+    END IF;
+
+    CREATE TABLE IF NOT EXISTS silex.users  (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, firstname char(50), lastname char(50));
+
+    IF TABLE_POPULATED = 0 THEN
+        INSERT INTO silex.users (firstname, lastname) VALUES ('Harrison', 'Ford') , ('Georges', 'Clooney'), ('Julia', 'Roberts');
+    END IF;
+
+END //
+DELIMITER ;
+
+CALL Init;
+DROP PROCEDURE IF EXISTS Init;
+
+UPDATE mysql.user SET Password=PASSWORD('root') WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
