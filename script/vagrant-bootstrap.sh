6df11747b4c72d6d0c3f13207b342d5132004702
Julien Grossiord <jgrossiord@mediastay.com>
2013-06-04 18:04:15 +0200

    Setup the install parameters fix #1

--- script/vagrant-bootstrap.sh
+++ script/vagrant-bootstrap.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
 
+
+export DEBIAN_FRONTEND=noninteractive
+
 sudo apt-get update -y
+echo grub-pc grub-pc/install_devices multiselect /dev/sda | sudo debconf-set-selections
+echo grub-pc grub-pc/install_devices_disks_changed multiselect /dev/sda | sudo debconf-set-selections
+
 sudo apt-get upgrade -y -o dir::cache::archives="/vagrant/logs/apt-cache"
--- script/vagrant-db-bootstrap.sh
+++ script/vagrant-db-bootstrap.sh
@@ -2,11 +2,11 @@
 
 bash /vagrant/script/vagrant-bootstrap.sh
 
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -y -q mysql-server
+echo mysql-server-5.5 mysql-server/root_password password root | sudo debconf-set-selections
+echo mysql-server-5.5 mysql-server/root_password_again password root | sudo debconf-set-selections
+sudo apt-get install -y  mysql-server-5.5
 
-mysql -u root < /vagrant/script/db-init.sql
-
+mysql -u root -proot < /vagrant/script/db-init.sql
 
 ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v '10.0.2' | cut -d: -f2 | awk '{ print $1}' > /vagrant/logs/db/urls.txt
 echo "You can access mySQL on IP "
