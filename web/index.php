6cdab5b141873f7c82a02d9d8b2c608691bad8bb
Julien Grossiord <jgrossiord@mediastay.com>
2013-06-05 14:56:10 +0200

    Added base rendering of user list

--- web/index.php
+++ web/index.php
@@ -2,6 +2,7 @@
 require_once __DIR__.'/../vendor/autoload.php'; 
 
 $app = new Silex\Application();
+$app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../src/views',
 ));
@@ -9,18 +10,41 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__ . '/../logs/silex/development.log',
 ));
 
-$app->get('/', function() {
-    return "Hello World!";
+$app->get('/', function() use($app) {
+    return $app['twig']->render('index.twig');
 });
 $app->get('/hello/{name}', function ($name) use($app) {
     return $app['twig']->render('hello.twig', array(
         'name' => $name,
     ));
 });
-$app->get('/users', function () use($app) {
 
+$users = array(
+    1 => array(
+    	'id' => '1',
+        'firstname' => 'Julien',
+        'lastname'  => 'Grossiord'
+    ),
+    2 => array(
+    	'id' => '2',
+        'firstname' => 'John',
+        'lastname'  => 'Doe'
+    )
+);
+$app->get('/users', function () use($app, $users) {
     return $app['twig']->render('users.twig', array(
     	'users' => $users,
     ));
 });
+$app->get('/users/{id_user}', function ($id_user) use($app, $users) {
+    return $app['twig']->render('user.twig', array(
+    	'user' => $users[$id_user],
+    ));
+});
+	$foo = new \Foo\Foo();
+
+$app->get('/{q}', function() use ($app, $foo) {
+    return $foo->routeDumper($app['routes']->getIterator());
+});
+
 $app->run();
