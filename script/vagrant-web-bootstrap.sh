243b769ec9095a7499a5f57b4868be63d0f38c65
Julien Grossiord <jgrossiord@mediastay.com>
2013-06-06 13:38:23 +0200

    Install PHP Unit

--- script/vagrant-web-bootstrap.sh
+++ script/vagrant-web-bootstrap.sh
@@ -19,6 +19,9 @@ curl -s http://getcomposer.org/installer | php5
 php5 composer.phar install
 php5 composer.phar update
 
+sudo pear config-set auto_discover 1
+sudo pear install pear.phpunit.de/PHPUnit
+
 
 ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v '10.0.2' | grep -v '10.11.12.1' | cut -d: -f2 | awk '{ print "http://"$1"/"}' > /vagrant/logs/web/urls.txt
 echo "You can access your application on "
