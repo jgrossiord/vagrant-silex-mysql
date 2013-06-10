ad172a13e7c5989c51a9ef2b46e77ed2c19b7960
Julien Grossiord <jgrossiord@mediastay.com>
2013-06-10 12:05:36 +0200

    Enable network connection to mySQL Fix #5

--- script/vagrant-db-bootstrap.sh
+++ script/vagrant-db-bootstrap.sh
@@ -6,6 +6,10 @@ echo mysql-server-5.5 mysql-server/root_password password root | sudo debconf-se
 echo mysql-server-5.5 mysql-server/root_password_again password root | sudo debconf-set-selections
 sudo apt-get install -y  -o dir::cache::archives="/vagrant/logs/apt-cache" mysql-server-5.5
 
+# disable bind-address to enable connection from other computers
+sudo sed -i -e 's/^\(bind-address\)/#\1/g' /etc/mysql/my.cnf
+sudo servie mysql restart
+
 mysql -u root -proot < /vagrant/script/db-init.sql
 
 ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | grep -v '10.0.2' | cut -d: -f2 | awk '{ print $1}' > /vagrant/logs/db/urls.txt
